49aa71cf34887a0fc8ccfdc53b048795bd4bea70
Aditya Ratna Shakya <aditya.shakya@r3.com>
2024-05-07 02:52:44 +0100

    Add probuilder

---
+++ Assets/Scripts/Exercise 6/PlayerController6.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                MovePlayer(hit.point);
+            };
+        }
+    }
+
+    void MovePlayer(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+}
